95b7f7a249ac000ab8bc9721d76506d25e4cbf35
unknown <justinasve@gmail.com>
2022-09-30 01:14:41 +0300

    second task done

--- TypeScript-lesson-2/užduotys/TS-2-typed-functions/src/main.ts
+++ TypeScript-lesson-2/užduotys/TS-2-typed-functions/src/main.ts
@@ -49,21 +49,21 @@ printRedText('printRed funkcijos argumentas');
 
 console.group('1. Aprašykite funkcijoms tipus jas deklaruojant');
 {
-  const multiply = (a, b) => {
+  const multiply = (a: number, b: number): number => {
     return a * b;
   };
 
-  const power = function (base, power) {
+  const power = function (base: number, power: number): number {
     return base ** power;
   };
 
-  const squareRoot = (number) => number ** (1 / 2);
+  const squareRoot = (number: number): number => number ** (1 / 2);
 
-  const root = function (base, nthRoot) {
+  const root = function (base: number, nthRoot: number): number {
     return base ** (1 / nthRoot);
   }
 
-  const printBlueText = (text) => console.log(`%c${text}`, 'color: #0000ee');
+  const printBlueText = (text: string): void => console.log(`%c${text}`, 'color: #0000ee');
 
   const num1: number = 16;
   const num2: number = 4;
@@ -82,21 +82,25 @@ console.groupEnd();
 
 console.group('2. Aprašykite funkcijų tipus prieš deklaruojant funkcijas');
 {
-  const multiply = (a, b) => {
+  type UnaryNumberProductNumberFunction = (number: number) => number;
+  type UnaryStringProductVoidFunction = (str: string) => void;
+  type BinaryNumberProductNumberFunction = (num1: number, num2: number) => number;
+
+  const multiply: BinaryNumberProductNumberFunction = (a, b) => {
     return a * b;
   };
 
-  const power = function (base, power) {
+  const power: BinaryNumberProductNumberFunction = function (base, power) {
     return base ** power;
   };
 
-  const squareRoot = (number) => number ** (1 / 2);
+  const squareRoot: UnaryNumberProductNumberFunction = (number) => number ** (1 / 2);
 
-  const root = function (base, nthRoot) {
+  const root: BinaryNumberProductNumberFunction = function (base, nthRoot) {
     return base ** (1 / nthRoot);
   }
 
-  const printBlueText = (text) => console.log(`%c${text}`, 'color: #0000ee');
+  const printBlueText: UnaryStringProductVoidFunction = (text) => console.log(`%c${text}`, 'color: #0000ee');
 
   const num1: number = 16;
   const num2: number = 4;
--- TypeScript-lesson-2/užduotys/TS-2-typed-objects/src/main.ts
+++ TypeScript-lesson-2/užduotys/TS-2-typed-objects/src/main.ts
@@ -53,7 +53,7 @@ printCouple(person1, person2);
 // Atlikite užduotis, funkcijas aprašydami tipais
 console.group('1. Sukurkite funkciją kuri patikrina ar žmogus yra pilnametis');
 {
-  const isAdult = () => {};
+  const isAdult = (p: Person): boolean => p.age >= 18;
   
   console.log({
     [createFullname(person1)]: isAdult(person1),
@@ -65,7 +65,9 @@ console.groupEnd();
 
 console.group('2. Sukurkite funkciją, kuri patikrina ar Person tipo objektas turi ūgį ir svorį');
 {
-  const isFullyDescribedPerson = () => {};
+  type IsFullyDescribedPerson = (p: Person) => boolean
+
+  const isFullyDescribedPerson: IsFullyDescribedPerson = (person) => Boolean(person.height) && Boolean(person.weight);
   
   console.log({
     [createFullname(person1)]: isFullyDescribedPerson(person1),
@@ -77,7 +79,7 @@ console.groupEnd();
 
 console.group('3. Sukurkite funkciją, kuri grąžina žmogaus incialus');
 {
-  const createInitials = () => {};
+  const createInitials = (p: Person) => p.name[0] + p.surname[0];
   
   console.log({
     [createFullname(person1)]: createInitials(person1),
